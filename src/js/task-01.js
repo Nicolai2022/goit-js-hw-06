a294c90a98a943bda6feec71ca83fb810f2517de
Mykola V <98426734+Nicolai2022@users.noreply.github.com>
2022-12-11 19:36:26 +0200

    js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,10 +2,14 @@ const cart = [54, 28, 105, 70, 92, 17, 120];
 let total = 0;
 // console.table(cart);
 
-for (let i = 0; i < cart.length; i += 1) {
-    console.log (cart [i]);
+// for (let i = 0; i < cart.length; i += 1) {
+//     console.log (cart [i]);
 
-    total += cart [i];
+//     total += cart [i];
+// }
+
+for (const value of cart) {
+    total += value;
 }
 
 console.log ('Total: ',total);
